30154b65fa787308495bc26cedfbc4f9d1a43b9b
Hu Xiang <johnhu456@163.com>
2017-11-08 22:33:07 +0800

    Config for drag item

--- DragAndDropResearch/DragAndDropResearch/DragToTransportViewController.swift
+++ DragAndDropResearch/DragAndDropResearch/DragToTransportViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import AVKit
 
 
-class DragToTransportViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+class DragToTransportViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,UITableViewDragDelegate {
     private let kCellReuseIdentifer = "kCellReuseIdentifer"
     
     private var tableView : UITableView!
@@ -31,6 +31,8 @@ class DragToTransportViewController: UIViewController,UITableViewDelegate,UITabl
         tableView = UITableView.init(frame: view.frame)
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.dragDelegate = self
+        tableView.dragInteractionEnabled = true
         tableView.register(DragToTransportCell.self, forCellReuseIdentifier: kCellReuseIdentifer)
         view.addSubview(tableView)
     }
@@ -38,10 +40,6 @@ class DragToTransportViewController: UIViewController,UITableViewDelegate,UITabl
         let videoUrl = NSURL.init(string:"http://111.1.50.88/mp4files/5100000004A4FB36/clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
         videoPlayItem = AVPlayerItem.init(url: videoUrl! as URL)
         videoPlayer = AVPlayer.init(playerItem: videoPlayItem)
-//        videoPlayItem.addObserver(self, forKeyPath: "status", options: .new, context: nil)
-//        videoPlayerLayer = AVPlayerLayer.init(player: videoPlayer)
-//        videoPlayerLayer.frame = view.frame
-//        videoPlayView = UIView.init(frame: view.frame)
     }
     
     func setupDragItem() {
@@ -72,8 +70,6 @@ class DragToTransportViewController: UIViewController,UITableViewDelegate,UITabl
             //Drag url
             cell = DragToTransportCell.init(videoPlayer: videoPlayer, reuseIdentifier: kCellReuseIdentifer)
             cell.videoPlayer = self.videoPlayer
-//            cell.contentView.backgroundColor = UIColor.red
-//            cell.detailTextLabel?.text = "Drag URL"
         default:
             //Drag video
             cell.contentView.backgroundColor = UIColor.green
@@ -86,5 +82,26 @@ class DragToTransportViewController: UIViewController,UITableViewDelegate,UITabl
         return 128
     }
     
-    //MARK: - Helper
+    //MARK: - UITableViewDragDelegate
+    
+    func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        let targetCell = tableView.cellForRow(at: indexPath) as! DragToTransportCell
+        let itemProvider = NSItemProvider.init(item: "2" as NSSecureCoding, typeIdentifier: "public.mpeg-4")
+
+        itemProvider.registerItem(forTypeIdentifier: "public.mpeg-4") { (loader, data, option) in
+            let request = URLRequest.init(url: NSURL.init(string:"http://111.1.50.88/mp4files/5100000004A4FB36/clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")! as URL)
+            let task = URLSession.shared.downloadTask(with: request, completionHandler: { (url, response, error) in
+                let data2 = NSData.init(contentsOf: url!)
+                print(url,response,error)
+                loader(data2,nil)
+            })
+            task.resume()
+        }
+        
+//        itemProvider.registerItem(forTypeIdentifier: "s") { (<#NSItemProvider.CompletionHandler!#>, <#AnyClass!#>, <#[AnyHashable : Any]!#>) in
+//            <#code#>
+//        }
+        let dragItem = UIDragItem.init(itemProvider: itemProvider)
+        return [dragItem]
+    }
 }
